5185171f5a7cc024d6f076316b458cf79fd99f85
Fongvrt24 <veeratorn.tre@student.mahidol.edu>
2024-10-20 20:36:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 # YOUR CODE HERE
+n=int(input())
+list1=[]
+list2=[]
+for i in range(n):
+    list1.append(int(input()))
+for i in range(n):
+    list2.append(int(input()))
+def summation (a,b):
+    listsum=[]
+    global n 
+    for i in range(n):
+        listsum.append(a[i]+b[i])
+    return listsum
+listsum=summation(list1,list2)
+def find_min_max(a):
+    min=a[0]
+    max=a[0]
+    for i in a:
+        if i<min:
+            min=i
+        else:
+            continue
+    for i in a:
+        if i>max:
+            max=i
+        else:
+            continue
+    min_max = (min,max)
+    return min_max
+print(summation(list1,list2))
+print(find_min_max(listsum))
